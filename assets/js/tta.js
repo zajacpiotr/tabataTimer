0f1cf8a1d30400a111aef3493af7f54453ff2895
zajacpiotr <33210182+zajacpiotr@users.noreply.github.com>
2018-02-09 00:12:43 +0100

    Added vis.js to show graph of weight

--- assets/js/tta.js
+++ assets/js/tta.js
@@ -273,6 +273,43 @@ function timerApp() {
             }
         }
     }
+
+    function graph() {
+        var container = document.getElementById("graphConteiner");
+        var items = [
+            {
+                x: '2014-06-11',
+                y: 10
+            },
+            {
+                x: '2014-06-12',
+                y: 25
+            },
+            {
+                x: '2014-06-13',
+                y: 30
+            },
+            {
+                x: '2014-06-14',
+                y: 10
+            },
+            {
+                x: '2014-06-15',
+                y: 15
+            },
+            {
+                x: '2014-06-16',
+                y: 50
+            }
+  ];
+
+        var dataset = new vis.DataSet(items);
+        var options = {
+            start: '2014-06-10',
+            end: '2014-06-18'
+        };
+        var graph2d = new vis.Graph2d(container, dataset, options);
+    }
     document.querySelector(".redMonthBtn").addEventListener("click", function () {
         month--;
         if (month < 0) {
@@ -313,6 +350,7 @@ function timerApp() {
         calendar(".monthHeader", ".calendarInner", ".filler");
         dayActive();
         weightAdding();
+        graph();
     }, false)
 
     document.querySelector(".redSecWBtn").addEventListener("click", function () {
